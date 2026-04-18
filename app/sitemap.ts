19716068bb776d19a10aec5f32554fff244c955a
ADQUIEREMAS <adquieremas@gmail.com>
2026-04-18 18:31:06 -0500

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,25 +2,23 @@ export default function sitemap() {
   const baseUrl = "https://clipnexo.com";
 
   const routes = [
+    "/es/acerca-de",
     "/es/descargar-tiktok",
     "/es/descargar-tiktok-mp3",
+    "/es/como-descargar-videos-de-tiktok",
     "/es/descargar-tiktok-sin-marca",
 
+    "/en/acerca-de",
     "/en/descargar-tiktok",
     "/en/descargar-tiktok-mp3",
+    "/en/como-descargar-videos-de-tiktok",
     "/en/descargar-tiktok-sin-marca",
 
+    "/pt/acerca-de",
     "/pt/descargar-tiktok",
     "/pt/descargar-tiktok-mp3",
+    "/pt/como-descargar-videos-de-tiktok",
     "/pt/descargar-tiktok-sin-marca",
-
-    "/pt/acerca-de",
-    "/pt/acerca-de",
-    "/pt/acerca-de",
-
-    "/pt/como-descargar-videos-de-tiktok",
-    "/pt/como-descargar-videos-de-tiktok",
-    "/pt/como-descargar-videos-de-tiktok",
   ];
 
   return routes.map((route) => ({
